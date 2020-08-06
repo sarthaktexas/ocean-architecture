e33060b72a8611c6dc51306f1080bc2f91937072
Sarthak Mohanty <sarthaktexas@gmail.com>
2020-08-06 02:05:00 -0500

    💳 created payment handler

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,29 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Auth0 Webapp sample Nodejs' });
+router.get('/', async function (req, res, next) {
+  try {
+    var stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      line_items: [{
+        price: process.env.STRIPE_PRICE_ID,
+        quantity: 1
+      }],
+      mode: "subscription",
+      success_url: "https://oceanaio.com/dashboard",
+      cancel_url: "https://oceanaio.com/"
+    });
+    res.render('index', {
+      title: "OceanAIO.com | Best group for methods",
+      sessionID: session.id
+    });
+  } catch (error) {
+    console.log(error);
+    res.render("index", {
+      title: "OceanAIO.com | Best group for methods"
+    });
+  }
 });
 
 module.exports = router;
