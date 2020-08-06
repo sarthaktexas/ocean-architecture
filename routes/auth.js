3dea4dffcb229ced5389bec14380d0dacae85d21
Sarthak Mohanty <sarthaktexas@gmail.com>
2020-08-06 17:03:39 -0500

    🚑 ugh hotfixes are hard and boring

--- routes/auth.js
+++ routes/auth.js
@@ -24,7 +24,7 @@ router.get('/callback', function (req, res, next) {
       if (err) { return next(err); }
       const returnTo = req.session.returnTo;
       delete req.session.returnTo;
-      res.redirect(returnTo || 'https://oceanaio.com/dashboard');
+      res.redirect(returnTo || '/dashboard');
     });
   })(req, res, next);
 });
