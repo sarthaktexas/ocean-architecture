5a5a7d3d9944c5fecb22101a640b3980f338b3d7
Sarthak Mohanty <sarthaktexas@gmail.com>
2020-08-08 01:19:59 -0500

    ✨ introduce billing portal

--- routes/index.js
+++ routes/index.js
@@ -106,12 +106,13 @@ router.post('/webhook', bodyParser.raw({
       break;
     case 'invoice.payment_failed':
       const paymentFail = event.data.object;
+      
       // Send Discord Webhook event
 
       const failembed = new Discord.MessageEmbed()
         .setTitle('Failed Payment!')
         .setColor('#DE1738')
-        .setDescription(paymentFail.customer_name + ' (*' + paymentFail.customer_email + '*) has not paid for the month. Delete thier account in the Auth0 Dashboard.');
+        .setDescription(paymentFail.customer_name + ' (*' + paymentFail.customer_email + '*) has not paid for the month. Delete thier account in the Auth0 Dashboard and remove them from Discord.');
 
       webhookClient.send({
         username: 'Failure Bot',
@@ -119,8 +120,39 @@ router.post('/webhook', bodyParser.raw({
         embeds: [failembed],
       });
       break;
-    case 'invoice.payment_action_required':
-      const paymentRequired = event.data.object;
+    case 'subscription_schedule.canceled':
+      const subscriptionEvent = event.data.object;
+      var headers = {
+        'Authorization': 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImxlcnlnSzB2d1hlUTRYMjVwbHVidCJ9.eyJpc3MiOiJodHRwczovL29jZWFuYWlvLnVzLmF1dGgwLmNvbS8iLCJzdWIiOiJ0Q2hEcHZXend3OUU3aHk1aEp1QXJFeEg2NXpSNGloWkBjbGllbnRzIiwiYXVkIjoiaHR0cHM6Ly9vY2VhbmFpby51cy5hdXRoMC5jb20vYXBpL3YyLyIsImlhdCI6MTU5Njg1NDcyNCwiZXhwIjoxNTk2OTQxMTI0LCJhenAiOiJ0Q2hEcHZXend3OUU3aHk1aEp1QXJFeEg2NXpSNGloWiIsInNjb3BlIjoicmVhZDpjbGllbnRfZ3JhbnRzIGNyZWF0ZTpjbGllbnRfZ3JhbnRzIGRlbGV0ZTpjbGllbnRfZ3JhbnRzIHVwZGF0ZTpjbGllbnRfZ3JhbnRzIHJlYWQ6dXNlcnMgdXBkYXRlOnVzZXJzIGRlbGV0ZTp1c2VycyBjcmVhdGU6dXNlcnMgcmVhZDp1c2Vyc19hcHBfbWV0YWRhdGEgdXBkYXRlOnVzZXJzX2FwcF9tZXRhZGF0YSBkZWxldGU6dXNlcnNfYXBwX21ldGFkYXRhIGNyZWF0ZTp1c2Vyc19hcHBfbWV0YWRhdGEgcmVhZDp1c2VyX2N1c3RvbV9ibG9ja3MgY3JlYXRlOnVzZXJfY3VzdG9tX2Jsb2NrcyBkZWxldGU6dXNlcl9jdXN0b21fYmxvY2tzIGNyZWF0ZTp1c2VyX3RpY2tldHMgcmVhZDpjbGllbnRzIHVwZGF0ZTpjbGllbnRzIGRlbGV0ZTpjbGllbnRzIGNyZWF0ZTpjbGllbnRzIHJlYWQ6Y2xpZW50X2tleXMgdXBkYXRlOmNsaWVudF9rZXlzIGRlbGV0ZTpjbGllbnRfa2V5cyBjcmVhdGU6Y2xpZW50X2tleXMgcmVhZDpjb25uZWN0aW9ucyB1cGRhdGU6Y29ubmVjdGlvbnMgZGVsZXRlOmNvbm5lY3Rpb25zIGNyZWF0ZTpjb25uZWN0aW9ucyByZWFkOnJlc291cmNlX3NlcnZlcnMgdXBkYXRlOnJlc291cmNlX3NlcnZlcnMgZGVsZXRlOnJlc291cmNlX3NlcnZlcnMgY3JlYXRlOnJlc291cmNlX3NlcnZlcnMgcmVhZDpkZXZpY2VfY3JlZGVudGlhbHMgdXBkYXRlOmRldmljZV9jcmVkZW50aWFscyBkZWxldGU6ZGV2aWNlX2NyZWRlbnRpYWxzIGNyZWF0ZTpkZXZpY2VfY3JlZGVudGlhbHMgcmVhZDpydWxlcyB1cGRhdGU6cnVsZXMgZGVsZXRlOnJ1bGVzIGNyZWF0ZTpydWxlcyByZWFkOnJ1bGVzX2NvbmZpZ3MgdXBkYXRlOnJ1bGVzX2NvbmZpZ3MgZGVsZXRlOnJ1bGVzX2NvbmZpZ3MgcmVhZDpob29rcyB1cGRhdGU6aG9va3MgZGVsZXRlOmhvb2tzIGNyZWF0ZTpob29rcyByZWFkOmFjdGlvbnMgdXBkYXRlOmFjdGlvbnMgZGVsZXRlOmFjdGlvbnMgY3JlYXRlOmFjdGlvbnMgcmVhZDplbWFpbF9wcm92aWRlciB1cGRhdGU6ZW1haWxfcHJvdmlkZXIgZGVsZXRlOmVtYWlsX3Byb3ZpZGVyIGNyZWF0ZTplbWFpbF9wcm92aWRlciBibGFja2xpc3Q6dG9rZW5zIHJlYWQ6c3RhdHMgcmVhZDp0ZW5hbnRfc2V0dGluZ3MgdXBkYXRlOnRlbmFudF9zZXR0aW5ncyByZWFkOmxvZ3MgcmVhZDpzaGllbGRzIGNyZWF0ZTpzaGllbGRzIHVwZGF0ZTpzaGllbGRzIGRlbGV0ZTpzaGllbGRzIHJlYWQ6YW5vbWFseV9ibG9ja3MgZGVsZXRlOmFub21hbHlfYmxvY2tzIHVwZGF0ZTp0cmlnZ2VycyByZWFkOnRyaWdnZXJzIHJlYWQ6Z3JhbnRzIGRlbGV0ZTpncmFudHMgcmVhZDpndWFyZGlhbl9mYWN0b3JzIHVwZGF0ZTpndWFyZGlhbl9mYWN0b3JzIHJlYWQ6Z3VhcmRpYW5fZW5yb2xsbWVudHMgZGVsZXRlOmd1YXJkaWFuX2Vucm9sbG1lbnRzIGNyZWF0ZTpndWFyZGlhbl9lbnJvbGxtZW50X3RpY2tldHMgcmVhZDp1c2VyX2lkcF90b2tlbnMgY3JlYXRlOnBhc3N3b3Jkc19jaGVja2luZ19qb2IgZGVsZXRlOnBhc3N3b3Jkc19jaGVja2luZ19qb2IgcmVhZDpjdXN0b21fZG9tYWlucyBkZWxldGU6Y3VzdG9tX2RvbWFpbnMgY3JlYXRlOmN1c3RvbV9kb21haW5zIHVwZGF0ZTpjdXN0b21fZG9tYWlucyByZWFkOmVtYWlsX3RlbXBsYXRlcyBjcmVhdGU6ZW1haWxfdGVtcGxhdGVzIHVwZGF0ZTplbWFpbF90ZW1wbGF0ZXMgcmVhZDptZmFfcG9saWNpZXMgdXBkYXRlOm1mYV9wb2xpY2llcyByZWFkOnJvbGVzIGNyZWF0ZTpyb2xlcyBkZWxldGU6cm9sZXMgdXBkYXRlOnJvbGVzIHJlYWQ6cHJvbXB0cyB1cGRhdGU6cHJvbXB0cyByZWFkOmJyYW5kaW5nIHVwZGF0ZTpicmFuZGluZyBkZWxldGU6YnJhbmRpbmcgcmVhZDpsb2dfc3RyZWFtcyBjcmVhdGU6bG9nX3N0cmVhbXMgZGVsZXRlOmxvZ19zdHJlYW1zIHVwZGF0ZTpsb2dfc3RyZWFtcyBjcmVhdGU6c2lnbmluZ19rZXlzIHJlYWQ6c2lnbmluZ19rZXlzIHVwZGF0ZTpzaWduaW5nX2tleXMgcmVhZDpsaW1pdHMgdXBkYXRlOmxpbWl0cyIsImd0eSI6ImNsaWVudC1jcmVkZW50aWFscyJ9.PoUIrrBlHGPvaPPMC6cVbimRch9Qd_TvHM50VCzFleh0nAbtNotiF_RGJlh0d9wQMrSRfvcRTfD0YDQuIBsXHMGwUcDCnvfJJqo0j7SEcdeDQ7Z642QnxBYpZIpjjCwMQmD0-PHyaDfwuKjSceOs3Zt66Nu2pFjMJ38IPcIx_xBzD-IkWpyVT4bHU8DW4iZ80jJY-P9lo0qWie3t5WjGhXu877qw1zzXljvEkNWCX7qW2hd1vp-TOlDNEoXQCDwnVmjGSU162Cc0RTJRg8Win4WKZTFc9e567WR1j7fDV0VGs6N-BaBayXAEaZXV8bLambVKJj1ot44pztu2JuKyWA',
+        'Content-Type': 'application/json'
+      };
+
+      function callback(error, response, body) {
+        if (!error && response.statusCode == 200) {
+          console.log(body);
+        }
+      }
+
+      const deleteUrl = 'https://oceanaio.us.auth0.com/api/v2/users/' + subscriptionEvent.customer
+
+      fetch(deleteUrl, {
+          method: 'DELETE',
+        })
+        .then(res => res.json())
+        .then(json => console.log(json));
+
+      // Send Discord Webhook event
+
+      const cancelembed = new Discord.MessageEmbed()
+        .setTitle('Cancel Subscription!')
+        .setColor('#DE1738')
+        .setDescription(paymentFail.customer + '*) has cancelled thier account. Delete thier account in the Stripe Dashboard and remove them on Discord.');
+
+      webhookClient.send({
+        username: 'Cancel Bot',
+        avatarURL: 'https://www.logolynx.com/images/logolynx/cc/cc69b8e3f205d98a6586a715df8f9b92.gif',
+        embeds: [cancelembed],
+      });
       break;
     case 'invoice.paid':
       const invoicePaid = event.data.object;
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var secured = require('../lib/middleware/secured');
 var router = express.Router();
+var stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 /* GET dashboard. */
 router.get('/dashboard', secured(), function (req, res, next) {
@@ -10,5 +11,22 @@ router.get('/dashboard', secured(), function (req, res, next) {
     title: 'OceanAIO.com | Best group for methods'
   });
 });
+
+router.post('/portal', secured(), async function (req, res, next) {
+  try {
+  const { _raw, _json, ...userProfile } = req.user;
+  const profile = JSON.stringify(userProfile, null, 2);
+  const id = req.user.id.substring(6);
+  console.log(id);
+  var session = await stripe.billingPortal.sessions.create({
+    customer: id,
+    return_url: process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/dashboard',
+  });
+  console.log(session.url);
+  res.redirect(session.url);
+} catch (error) {
+  console.log(error);
+}
+})
 
 module.exports = router;
