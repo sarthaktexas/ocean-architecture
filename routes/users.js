c8e518a3a7f4a60a1cec5a51cd100222ff6527a8
Sarthak Mohanty <sarthaktexas@gmail.com>
2020-08-10 17:56:00 -0500

    🚑 fix 404 page on dashboard

--- routes/users.js
+++ routes/users.js
@@ -28,10 +28,17 @@ router.post('/portal', secured(), async function (req, res, next) {
     const profile = JSON.stringify(userProfile, null, 2);
     const id = req.user.id.substring(6);
     console.log(id);
-    var session = await stripe.billingPortal.sessions.create({
-      customer: id,
-      return_url: process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/dashboard',
-    });
+    if (app.get('env') === 'production') {
+      var session = await stripe.billingPortal.sessions.create({
+        customer: id,
+        return_url: 'https://oceanaio.com/dashboard',
+      });
+    } else {
+      var session = await stripe.billingPortal.sessions.create({
+        customer: id,
+        return_url: 'http://localhost:3000/dashboard',
+      });
+    }
     console.log(session.url);
     res.redirect(session.url);
   } catch (error) {
